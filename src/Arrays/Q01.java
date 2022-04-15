9318c5b4b43c6d2b62182108bd19b82a4dababa6
Ramazan Yasar <ramazanyas@gmail.com>
2022-04-15 13:41:28 +0200

    15/04/2022

---
+++ src/Arrays/Q01.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class Q01 {
+    /* TASK :
+
+     * Input olarak verilen listteki isimlerden
+     * icinde ‘a’ harfi bulunanlari silen bir code create ediniz.
+     *
+     * INPUT : list1={"Ali","Veli","Ayse","Fatma","Omer"}
+     * OUTPUT : [Veli,Omer]
+     */
+
+
+}
